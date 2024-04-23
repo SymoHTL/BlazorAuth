263fb02e23146726102d8ded7608a79a7606bd0e
Symo <simon.hamanek@gmail.com>
2024-04-23 16:11:31 +0200

    update

--- Model/Configuration/ApplicationDbContext.cs
+++ Model/Configuration/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Model.Entities;
 
 namespace Model.Configuration;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options) {
+    public DbSet<ApplicationUser> ApplicationUsers{ get; set; }
     protected override void OnModelCreating(ModelBuilder builder) {
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
